26b08bb2f87fc30be79b93dd8d4e45c1d261463c
patsbro <brandenlatvala@gmail.com>
2026-01-26 16:45:39 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.log('[SCAtak] Service Worker Installed');
 });
 
 self.addEventListener('fetch', (e) => {
-  // This allows the app to load while offline in the future
   e.respondWith(fetch(e.request));
 });
