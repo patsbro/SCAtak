b55be8973cdf4bf1b2fc2541110adaa778c17436
patsbro <brandenlatvala@gmail.com>
2026-01-26 16:54:48 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener('install', (e) => {
-  console.log('[SCAtak] Service Worker Installed');
+const CACHE_NAME = 'scatak-v14';
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
